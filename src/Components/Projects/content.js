fb1bcbbe048f17cd461e9a966ea2d1af9cd6d56f
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2020-04-19 17:09:21 -0300

    update projects content

--- src/Components/Projects/content.js
+++ src/Components/Projects/content.js
@@ -1,17 +1,28 @@
 const projects = [
 	{
-		name: "Website Psicóloga Vanessa Mattos",
-		description: "An website with blog.",
-		tools: ["Figma", "PHP", "Wordpress"],
+		name: "My personal site (this)",
+		description:
+			"Created for fun and training skills with ReactJS. I chose the Windows 95 theme to provide a nostalgic experience.",
+		tools: ["ReactJS with React95 library", "Netlify"],
+		urls: {
+			sourceCode: "https://github.com/kaiofelipejs/personal-site",
+			appUrl: "",
+		},
+	},
+	{
+		name: "Remote HeartCount",
+		description: `In the HR (Human Resources) world, the indicator headcount represents the number of employees in the company. In my team, we decided to name it HeartCount. The HeartCount represents the number of hires/placements we do. We chose the word "heart" instead of "head", to reflect how much we care about our candidates.`,
+		tools: ["NodeJS", "React", "Knex", "Postgres", "Heroku"],
 		urls: {
 			sourceCode: "https://github.com/kaiofelipejs/psico-vanessa-mattos",
 			appUrl: "http://www.vanessamattos.com.br/",
 		},
 	},
 	{
-		name: "Remote HeartCount",
-		description: "An app for hiring team",
-		tools: ["Figma", "NodeJS", "React", "Knex", "Postgres"],
+		name: "Website Psicóloga Vanessa Mattos",
+		description:
+			"An institutional website with a blog for Psychologist Vanessa Mattos. This is my first project created from scratch (from design to hosting).",
+		tools: ["Figma", "PHP", "Javascript", "Wordpress"],
 		urls: {
 			sourceCode: "https://github.com/kaiofelipejs/psico-vanessa-mattos",
 			appUrl: "http://www.vanessamattos.com.br/",
