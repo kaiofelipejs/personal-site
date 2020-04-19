9a3b4270adddf3e36eb7f4423aada5a47b17fba2
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2020-04-19 17:15:41 -0300

    update career content

--- src/Components/Career/content.js
+++ src/Components/Career/content.js
@@ -3,7 +3,7 @@ const career = [
 		companyName: "Resultados Digitais",
 		job: "Tech Recruiter",
 		description:
-			"Entrevistas e phone screenings, sourcing de profissionais para Produto & Engenharia, relacionamento com a comunidade de tecnologia, ações de atração em mídias e eventos, realização de briefings, job posts e melhoria contínua nos processos com foco em Candidate Experience.",
+			"I contribute with Sourcing/Hunting of people to be part of our Product & Engineering team, attraction actions in media and events (this includes arts and landing pages), interviews, phone screenings, briefings, job posts and continuous improvement in processes focused on Candidate Experience.",
 		dates: {
 			startMonth: "April",
 			startYear: "2018",
@@ -15,7 +15,7 @@ const career = [
 		companyName: "Softplan",
 		job: "Young Apprentice to Administrative Analyst",
 		description:
-			"Planejamento/controle do inventário, requisições de compra, apresentação e negociação de orçamentos, automatização de planilhas e geração de indicadores. Apoio também na organização de eventos, demandas de compras e facilities, comunicação interna e onboarding de novos integrantes ao time.",
+			"Softplan is one of the largest companies in Brazil in the development of management software operating in different segments - Justice, Health, Public Management, and the Construction Industry. I contribute with the planning/control of the inventory, purchase requisitions, presentation of budgets, negotiation with suppliers, automation of apportionment of invoices and generation of indicators of the demands met by the team. Support also in organizing events, shopping and facilities demands, internal communication and onboarding new members to the Administrative team.",
 		dates: {
 			startMonth: "June",
 			startYear: "2015",
@@ -27,11 +27,23 @@ const career = [
 		companyName: "Cimes Construtora",
 		job: "Young Apprentice - Programmer",
 		description:
-			"A CIMES é uma das empresas apoiadoras do Programa de Aprendizagem Industrial do SENAI com auxílio para bolsistas. Minha principal responsabilidade era apresentar bons resultados das atividades desenvolvidas em sala e eventos. ",
+			"CIMES is one of the companies supporting the SENAI Industrial Learning Program with assistance for scholarship holders. My main responsibility was to present good results from the activities developed in the room and events.",
 		dates: {
 			startMonth: "February",
 			startYear: "2014",
 			endMonth: "December",
+			endYear: "2014",
+		},
+	},
+	{
+		companyName: "Costão Golf Club",
+		job: "Intern",
+		description:
+			"Receive players, perform equipment maintenance and support the organization of sporting events.",
+		dates: {
+			startMonth: "November",
+			startYear: "2013",
+			endMonth: "February",
 			endYear: "2014",
 		},
 	},
