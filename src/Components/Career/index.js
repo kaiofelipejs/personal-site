0c6357b6c953d53e1f806de14ae8522a4c303af7
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2020-04-21 14:10:02 -0300

    create education section in career component

--- src/Components/Career/index.js
+++ src/Components/Career/index.js
@@ -1,23 +1,42 @@
 import React from "react";
-import career from "./content";
+import { resume, education } from "./content";
 import { Divider } from "./styled";
 
 const Career = () => {
 	return (
 		<>
-			{career.map((c) => (
-				<div key={c.companyName}>
+			<h2>Resume</h2>
+			{resume.map((r) => (
+				<div key={r.companyName}>
 					<p>
-						<strong>{c.job}</strong> | {c.companyName}
+						<strong>{r.job}</strong> | {r.companyName}
 					</p>
 					<span>
-						{c.dates.startMonth}/{c.dates.startYear} -
-						{c.dates.endMonth === ""
+						{r.dates.startMonth}/{r.dates.startYear} -
+						{r.dates.endMonth === ""
 							? " Actual"
-							: ` ${c.dates.endMonth}/${c.dates.endYear}`}
+							: ` ${r.dates.endMonth}/${r.dates.endYear}`}
 					</span>
 
-					<p>{c.description}</p>
+					<p>{r.description}</p>
+
+					<Divider />
+				</div>
+			))}
+			<h2>Education</h2>
+			{education.map((e) => (
+				<div key={e.schoolName}>
+					<p>
+						<strong>
+							{e.degree} in {e.fieldStudy}
+						</strong>{" "}
+						| {e.schoolName}
+					</p>
+					<span>
+						{e.dates.startYear} - {e.dates.endYear}
+					</span>
+
+					<p>{e.description}</p>
 
 					<Divider />
 				</div>
