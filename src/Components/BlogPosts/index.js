9203e3153a39e2da716a99222260bcf5fd4a671b
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2020-04-28 23:14:05 -0300

    rename funcion getPosts

--- src/Components/BlogPosts/index.js
+++ src/Components/BlogPosts/index.js
@@ -6,7 +6,7 @@ const Posts = () => {
 	const [posts, setPosts] = useState([]);
 	const [loading, toggleLoading] = useState(false);
 
-	const getPosts = async () => {
+	const getAllPosts = async () => {
 		return fetch(
 			"https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@kaiofelipejs"
 		)
@@ -20,7 +20,7 @@ const Posts = () => {
 	useEffect(() => {
 		async function fetchData() {
 			toggleLoading(true);
-			const allPosts = await getPosts();
+			const allPosts = await getAllPosts();
 			const posts = allPosts.filter((item) => item.categories.length > 0);
 
 			setPosts(posts);
