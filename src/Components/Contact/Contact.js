0ebdd54af08d95727432ed1daa3c53874865f07c
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2020-05-15 20:41:59 -0300

    fix Contact component

---
+++ src/Components/Contact/Contact.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { links, email } from "./content";
+import Icons from "./Icons";
+import styled from "@xstyled/styled-components";
+
+const SocialLinksWrapper = styled.nav`
+	display: flex;
+	flex: 1;
+	justify-content: center;
+`;
+
+const SocialLinksList = styled.ul`
+	margin: 0;
+	padding: 0;
+	box-sizing: bordex-box;
+	display: flex;
+	list-style: none;
+`;
+
+const SocialLinksItem = styled.li``;
+
+const SocialLinksLink = styled.a`
+	display: inline-block;
+	text-align: center;
+	font-size: 20px;
+	padding: 10px;
+	color: inherit;
+	text-decoration: inherit;
+`;
+
+const Contact = () => (
+	<>
+		<h3>Let's talk!</h3>
+		<p>You can find me in these sites:</p>
+		<SocialLinksWrapper>
+			<SocialLinksList>
+				{links.map((link, i) => {
+					const Icon = Icons[link.label];
+
+					return (
+						<SocialLinksItem key={i}>
+							<SocialLinksLink
+								href={link.url}
+								title={link.label}
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<Icon />
+							</SocialLinksLink>
+						</SocialLinksItem>
+					);
+				})}
+			</SocialLinksList>
+		</SocialLinksWrapper>
+		<p>
+			Or sending me a message in:
+			<a href={`mailto:${email}`} target="_blank" rel="noopener noreferrer">
+				{` ${email}`}
+			</a>
+		</p>
+	</>
+);
+
+export default Contact;
