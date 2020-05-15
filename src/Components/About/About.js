1f2ee3a7ea575c8798b21ff6e678f3520b9f17a1
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2020-05-15 20:29:23 -0300

    refactor About component

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,7 +1,16 @@
-const about = {
+import React from "react";
+
+const content = {
 	title: "About me",
 	description:
 		"Passionate for technology, nerd, curious and always interested in learning new something. Most of my studies are focused on technology and in my free time I like development something (usually involving frontend), attend events, knowing different something and, somehow, contribute to the open-source world. Also, I lovely music and a good coffee. ",
 };
 
-export default about;
+const About = () => (
+	<>
+		<h3 data-testid="about-title">{content.title}</h3>
+		<p>{content.description}</p>
+	</>
+);
+
+export default About;
