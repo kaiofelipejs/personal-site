dec32e24e6882ce13c1a51dcaa4c779320a14ca5
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2020-10-02 19:59:19 -0300

    Fix url in Remote Hearcount project

--- src/Components/Projects/content.js
+++ src/Components/Projects/content.js
@@ -1,33 +1,33 @@
 const projects = [
-	{
-		name: "My personal site (this)",
-		description:
-			"Created for fun and training skills with ReactJS. I chose the Windows 95 theme to provide a nostalgic experience.",
-		tools: ["ReactJS", "React95 lib", "Netlify"],
-		urls: {
-			sourceCode: "https://github.com/kaiofelipejs/personal-site",
-			appUrl: "",
-		},
-	},
-	{
-		name: "Remote HeartCount",
-		description: `In the HR (Human Resources) world, the indicator headcount represents the number of employees in the company. In my team, we decided to name it HeartCount. The HeartCount represents the number of hires/placements we do. We chose the word "heart" instead of "head", to reflect how much we care about our candidates.`,
-		tools: ["NodeJS", "React", "Knex", "Postgres", "Heroku"],
-		urls: {
-			sourceCode: "https://github.com/kaiofelipejs/psico-vanessa-mattos",
-			appUrl: "http://www.vanessamattos.com.br/",
-		},
-	},
-	{
-		name: "Website Psicóloga Vanessa Mattos",
-		description:
-			"An institutional website with a blog for Psychologist Vanessa Mattos. This is my first project created from scratch (from design to hosting).",
-		tools: ["Figma", "PHP", "Javascript", "Wordpress"],
-		urls: {
-			sourceCode: "https://github.com/kaiofelipejs/psico-vanessa-mattos",
-			appUrl: "http://www.vanessamattos.com.br/",
-		},
-	},
+  {
+    name: "My personal site (this)",
+    description:
+      "Created for fun and training skills with ReactJS. I chose the Windows 95 theme to provide a nostalgic experience.",
+    tools: ["ReactJS", "React95 lib", "Netlify"],
+    urls: {
+      sourceCode: "https://github.com/kaiofelipejs/personal-site",
+      appUrl: "",
+    },
+  },
+  {
+    name: "Remote HeartCount",
+    description: `In the HR (Human Resources) world, the indicator headcount represents the number of employees in the company. In my team, we decided to name it HeartCount. The HeartCount represents the number of hires/placements we do. We chose the word "heart" instead of "head", to reflect how much we care about our candidates.`,
+    tools: ["NodeJS", "React", "Knex", "Postgres", "Heroku"],
+    urls: {
+      sourceCode: "https://github.com/kaiofelipejs/remoteheartcount-frontend",
+      appUrl: "https://remoteheartcount.herokuapp.com/",
+    },
+  },
+  {
+    name: "Website Psicóloga Vanessa Mattos",
+    description:
+      "An institutional website with a blog for Psychologist Vanessa Mattos. This is my first project created from scratch (from design to hosting).",
+    tools: ["Figma", "PHP", "Javascript", "Wordpress"],
+    urls: {
+      sourceCode: "https://github.com/kaiofelipejs/psico-vanessa-mattos",
+      appUrl: "http://www.vanessamattos.com.br/",
+    },
+  },
 ];
 
 export default projects;
